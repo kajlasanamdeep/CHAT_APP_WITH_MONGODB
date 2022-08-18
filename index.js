cfa1f46919ce1ae0b3a6444f1752744bb8be8edb
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-08-18 15:03:44 +0530

    cors origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sockets = require('./socket.io/sockets');
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin) return callback(null, true);
-        if (['http://localhost:3000'].indexOf(origin) === -1) {
+        if (['http://localhost:3000','http://localhost:3001','http://localhost:3002','http://localhost:3003'].indexOf(origin) === -1) {
             let msg = 'The CORS policy for this site does not allow access from the specified Origin.';
             return callback(new Error(msg), false);
         }
@@ -21,7 +21,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(routes);
-const io = require('socket.io')(server,{cors:{origin:'http://localhost:3000'}});
+const io = require('socket.io')(server,{cors:{origin:['http://localhost:3000','http://localhost:3001','http://localhost:3002','http://localhost:3003']}});
 io.on('connection',sockets);
 connection.connect().then((connected) => {
 
